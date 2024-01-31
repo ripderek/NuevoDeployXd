3a7d5341bd73fbf07596f4081842c47661bd2ab4
Raul <castillosteven09@outlook.es>
2024-01-31 15:14:44 -0500

    new changes443

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,3 +1,3 @@
 export * from '@/components/layout/BarraNavegacion2';
 export * from '@/components/layout/Navbar';
-export * from '@/components/layout/configurator';
+export * from '@/components/layout/Configurator';
